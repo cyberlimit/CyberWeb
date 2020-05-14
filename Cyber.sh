6b65f7d47ed469f30a2056e29227697d1ee51680
cyberlimit <64591373+cyberlimit@users.noreply.github.com>
2020-05-14 23:03:43 +0700

    Update Cyber.sh

--- Cyber.sh
+++ Cyber.sh
@@ -5,7 +5,7 @@
 #MikirMakeOtakGblk
 #KontolKaloMauReedit/Recode 
 #Pm Gua Bangsat
-clear
+clear 
 blue='\033[34;1m'
 green='\033[32;1m'  
 purple='\033[35;1m'
@@ -13,7 +13,7 @@ cyan='\033[36;1m'
 red='\033[31;1m'
 white='\033[37;1m'                                           
 yellow='\033[33;1m'
-sleep 1
+sleep 5
 echo 
 echo ""
 echo "\033[34;1m[$]\033[36;1m============================\033[34;1m[$]"
