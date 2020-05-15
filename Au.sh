4cb81d9f4d7340db7e37e0431ebced1f6ebbc329
cyberlimit <64591373+cyberlimit@users.noreply.github.com>
2020-05-16 01:24:16 +0700

    Create Au.sh

---
+++ Au.sh
@@ -0,0 +1,23 @@
+#!/system/xbin/Bash
+#JnganRecodeAnjg
+#BikinSendiri
+#Bangsatt
+#JanganLupaToxic✓
+
+clear 
+blue='\33[0;36m'
+green='\33[0;32m'  
+Yellow light='\33[1;33m'
+bluelight='\33[36;1m'
+red='\33[31;1m'
+white='\33[37;1m'                                           
+yellow='\33[33;1m'
+sleep 2
+echo "¥^=°=°=°°=°=°=°°=°=°°=°=°=°=°=°=°=°=°=°=°=°=°=°°=°=°=°°^¥"
+echo "\33[31;1mAuthor   : CyberLimit"
+echo "\33[33;1mkontak me: 089614686780"
+echo "\33[0;36mMy Team. : Wisnu | Panjull | Rehan"
+echo "\33[36;1mGithub.  : CyberLimit"
+echo "\33[0;32mYoutube. : Cyber Limit"
+echo "¥^=°=°=°=°=°=°=°=°°°=°=°==°=°=°=°=°=°=°=°=°°°=°°=°=°=°=^¥"
+echo ""
